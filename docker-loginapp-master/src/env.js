6a2287721d3b67a955542433ccb1cc50ae7b1ba3
Srinivas Katarapu <58047760+srkatarapu@users.noreply.github.com>
2021-05-08 23:29:59 +0530

    Delete docker-loginapp-master directory

--- docker-loginapp-master/src/env.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    msHost: process.env.MSHOST,
-    msDatabase: process.env.MSDATABASE,
-    msUser: process.env.MSUSER,
-    msPassword: process.env.MSPASSWORD,
-    serverPort: process.env.SERVERPORT
-  };
-  
